10ca8edc9df0ababc99d138ecc5626bb8da90738
Kozik <kozikbro@gmail.com>
2021-06-29 22:35:43 +0200

    better frontend for index blade

--- REST_API/resources/views/index.blade.php
+++ REST_API/resources/views/index.blade.php
@@ -4,17 +4,76 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
         <title>Emloyees</title>
     </head>
     <body>
-        <h1>List of employees</h1>
-        @foreach ($employees as $employee)
-            <p>ID: {{ $employee['id'] }}</p>
-            <p>Name: {{ $employee['name'] }}</p>
-            <p>Team: {{ $employee['team'] }}</p>
-            <p>Salary: {{ $employee['salary'] }}</p>
-            <br/>
-        @endforeach
-    </body0>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12 m-0 p-0">
+                    <div class="bg-primary text-white text-center">
+                        <h1 class="p-5">List of employees</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                @foreach ($employees as $employee)
+                    <div class="col-sm-6 col-md-4 col-lg-3"> 
+                        <div class="card mb-3">
+                            <div class="card-header bg-secondary text-white">
+                                <span><strong>{{ $employee['name'] }}</strong></span>
+                            </div>
+                            <div class="card-body">
+                                <p><strong>ID:</strong> {{ $employee['id'] }}</p>
+                                <p><strong>Team:</strong> {{ $employee['team'] }}</p>
+                                <p><strong>Salary:</strong> ${{ $employee['salary'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </body>
 </html>
